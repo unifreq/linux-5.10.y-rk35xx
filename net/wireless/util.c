3097a6b8de6de0493e76e2f0f75ba4292e40fa86
Arend van Spriel <arend.vanspriel@broadcom.com>
2021-01-05 11:58:39 +0100

    cfg80211: add VHT rate entries for MCS-10 and MCS-11
    Observed the warning in cfg80211_calculate_bitrate_vht() using an
    11ac chip reporting MCS-11. Since devices reporting non-standard
    MCS-9 is already supported add similar entries for MCS-10 and MCS-11.
    Actually, the value of MCS-9@20MHz is slightly off so corrected that.
    
    Signed-off-by: Arend van Spriel <arend.vanspriel@broadcom.com>

--- net/wireless/util.c
+++ net/wireless/util.c
@@ -1244,7 +1244,9 @@ static u32 cfg80211_calculate_bitrate_vht(struct rate_info *rate)
 		   65000000,
 		   78000000,
 		/* not in the spec, but some devices use this: */
-		   86500000,
+		   86700000,
+		   97500000,
+		  108300000,
 		},
 		{  13500000,
 		   27000000,
@@ -1256,6 +1258,8 @@ static u32 cfg80211_calculate_bitrate_vht(struct rate_info *rate)
 		  135000000,
 		  162000000,
 		  180000000,
+		  202500000,
+		  225000000,
 		},
 		{  29300000,
 		   58500000,
@@ -1267,6 +1271,8 @@ static u32 cfg80211_calculate_bitrate_vht(struct rate_info *rate)
 		  292500000,
 		  351000000,
 		  390000000,
+		  438800000,
+		  487500000,
 		},
 		{  58500000,
 		  117000000,
@@ -1278,12 +1284,14 @@ static u32 cfg80211_calculate_bitrate_vht(struct rate_info *rate)
 		  585000000,
 		  702000000,
 		  780000000,
+		  877500000,
+		  975000000,
 		},
 	};
 	u32 bitrate;
 	int idx;
 
-	if (rate->mcs > 9)
+	if (rate->mcs > 11)
 		goto warn;
 
 	switch (rate->bw) {
