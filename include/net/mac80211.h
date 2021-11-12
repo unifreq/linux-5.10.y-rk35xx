cc684965ce20501673afc6e2e4574279b4c4cea8
Felix Fietkau <nbd@nbd.name>
2021-11-12 12:22:23 +0100

    mac80211: add support for .ndo_fill_forward_path
    This allows drivers to provide a destination device + info for flow offload
    Only supported in combination with 802.3 encap offload
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    Tested-by: Lorenzo Bianconi <lorenzo@kernel.org>

--- include/net/mac80211.h
+++ include/net/mac80211.h
@@ -3959,6 +3959,8 @@ struct ieee80211_prep_tx_info {
  *	radar channel.
  *	The caller is expected to set chandef pointer to NULL in order to
  *	disable offchannel CAC/radar detection.
+ * @net_fill_forward_path: Called from .ndo_fill_forward_path in order to
+ *	resolve a path for hardware flow offloading
  */
 struct ieee80211_ops {
 	void (*tx)(struct ieee80211_hw *hw,
@@ -4289,6 +4291,11 @@ struct ieee80211_ops {
 				     struct ieee80211_sta *sta, u8 flowid);
 	int (*set_radar_offchan)(struct ieee80211_hw *hw,
 				 struct cfg80211_chan_def *chandef);
+	int (*net_fill_forward_path)(struct ieee80211_hw *hw,
+				     struct ieee80211_vif *vif,
+				     struct ieee80211_sta *sta,
+				     struct net_device_path_ctx *ctx,
+				     struct net_device_path *path);
 
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
