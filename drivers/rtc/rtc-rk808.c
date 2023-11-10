5ac2fb6a48c5b90d1a34f8b6f287b3418faccb37
Feng Zhang <feng@radxa.com>
2023-11-10 17:46:18 +0800

    drivers/rtc: rk808: rk809-rtc supports disability
    Signed-off-by: Feng Zhang <feng@radxa.com>

--- drivers/rtc/rtc-rk808.c
+++ drivers/rtc/rtc-rk808.c
@@ -417,6 +417,7 @@ static int rk808_rtc_probe(struct platform_device *pdev)
 	switch (rk808->variant) {
 	case RK805_ID:
 	case RK808_ID:
+	case RK809_ID:
 	case RK816_ID:
 	case RK817_ID:
 	case RK818_ID:
